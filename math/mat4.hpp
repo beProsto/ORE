d4a4452a83a2f909ef63dede4195608dab1446db
beProsto <lavionperavion2@gmail.com>
2020-04-14 21:18:57 +0200

    Hell yeah, file seperation!

--- math/mat4.hpp
+++ math/mat4.hpp
@@ -1,278 +1,91 @@
 #ifndef _ORE_MAT4_HPP_HEADER_FILE_GUARD
 #define _ORE_MAT4_HPP_HEADER_FILE_GUARD
 
-#include <cmath>
-#include <cstring>
-#include <utility>
+#include "_mat4.hpp"
 
-/* Matrix Operations */
-/* All of these require a pointer to float and name it _mat. 
-Some of these require a pointer to float and name it _in.
-These pointers should point to a predefined array of floats, with 16 elements. */
-namespace Math {
-namespace _Mat4 {
-	void Copy(float* _mat, const float* _in) {
-		memcpy(_mat, _in, 16 * sizeof(float));
+class Mat4 {
+public:
+	Mat4() {
+		_Mat4::Identity(m);
 	}
-	void Identity(float* _mat) {
-		memset(_mat, 0, 16 * sizeof(float));
-		_mat[0] = _mat[5] = _mat[10] = _mat[15] = 1.0f;
+	Mat4(float* _other) {
+		memcpy(m, _other, 16 * sizeof(float));
 	}
-	void Transpose(float* _mat) {
-		std::swap(_mat[1], _mat[4]);
-		std::swap(_mat[2], _mat[8]);
-		std::swap(_mat[3], _mat[12]);
-		std::swap(_mat[7], _mat[13]);
-		std::swap(_mat[11], _mat[14]);
-		std::swap(_mat[6], _mat[9]);
+	Mat4(const Mat4& _other) {
+		memcpy(m, _other.m, 16 * sizeof(float));
 	}
-	void Multiply(float* _mat, const float* _a, const float* _b) {
-		float* a = (float*)_a; 
-		bool aAllocated = false;
-		if(_a == _mat) {
-			a = new float[16];	
-			aAllocated = true;
-			memcpy(a, _a, 16 * sizeof(float));	
-		}
-
-		float b0 = _b[0],
-			b1 = _b[1],
-			b2 = _b[2],
-			b3 = _b[3];
-
-		_mat[0] = b0 * a[0] + b1 * a[4] + b2 * a[8] + b3 * a[12];
-		_mat[1] = b0 * a[1] + b1 * a[5] + b2 * a[9] + b3 * a[13];
-		_mat[2] = b0 * a[2] + b1 * a[6] + b2 * a[10] + b3 * a[14];
-		_mat[3] = b0 * a[3] + b1 * a[7] + b2 * a[11] + b3 * a[15];
-
-		b0 = _b[4];
-		b1 = _b[5];
-		b2 = _b[6];
-		b3 = _b[7];
-
-		_mat[4] = b0 * a[0] + b1 * a[4] + b2 * a[8] + b3 * a[12];
-		_mat[5] = b0 * a[1] + b1 * a[5] + b2 * a[9] + b3 * a[13];
-		_mat[6] = b0 * a[2] + b1 * a[6] + b2 * a[10] + b3 * a[14];
-		_mat[7] = b0 * a[3] + b1 * a[7] + b2 * a[11] + b3 * a[15];
-
-		b0 = _b[8];
-		b1 = _b[9];
-		b2 = _b[10];
-		b3 = _b[11];
-
-		_mat[8] = b0 * a[0] + b1 * a[4] + b2 * a[8] + b3 * a[12];
-		_mat[9] = b0 * a[1] + b1 * a[5] + b2 * a[9] + b3 * a[13];
-		_mat[10] = b0 * a[2] + b1 * a[6] + b2 * a[10] + b3 * a[14];
-		_mat[11] = b0 * a[3] + b1 * a[7] + b2 * a[11] + b3 * a[15];
-
-		b0 = _b[12];
-		b1 = _b[13];
-		b2 = _b[14];
-		b3 = _b[15];
-
-		_mat[12] = b0 * a[0] + b1 * a[4] + b2 * a[8] + b3 * a[12];
-		_mat[13] = b0 * a[1] + b1 * a[5] + b2 * a[9] + b3 * a[13];
-		_mat[14] = b0 * a[2] + b1 * a[6] + b2 * a[10] + b3 * a[14];
-		_mat[15] = b0 * a[3] + b1 * a[7] + b2 * a[11] + b3 * a[15];
+	~Mat4() {
 		
-		if(aAllocated) {
-			delete[] a;
-		}
 	}
-	void Perspective(float* _mat, float _fov, float _aspect, float _near, float _far) {
-		memset(_mat, 0, 16 * sizeof(float));
-		float frustumScale = 1.0 / std::tan(_fov / 2);
-		_mat[0] = frustumScale / _aspect;
-		_mat[5] = frustumScale;
-		_mat[10] = (_far + _near) / (_near - _far);
-		_mat[14] = (2 * _far * _near) / (_near - _far);
-		_mat[11] = -1.0f;
+	
+	float& operator[](int _i) {
+		return m[_i];		
 	}
-	void Orthographic(float* _mat, float _left, float _right, float _bottom, float _top, float _near, float _far) {
-		memset(_mat, 0, 16 * sizeof(float));
-		_mat[0] = 2.0f / (_right - _left);
-		_mat[5] = 2.0f / (_top - _bottom);
-		_mat[10] = -(2.0f / (_far - _near));
-		_mat[12] = -((_right + _left) / (_right - _left));
-		_mat[13] = -((_top + _bottom) / (_top - _bottom));
-		_mat[14] = -((_far + _near) / (_far - _near));
-		_mat[15] = 1.0f;
+	const float& operator[](int _i) const {
+		return m[_i];		
 	}
-	void Translate(float* _mat, float _x, float _y, float _z) {
-		_mat[12] += _x;
-		_mat[13] += _y;
-		_mat[14] += _z;
+	
+	Mat4& operator=(const Mat4& _other) {
+		memcpy(m, _other.m, 16 * sizeof(float));
+		return *this;
 	}
-	void Translate(float* _mat, const float* _in, float _x, float _y, float _z) {
-		_mat[12] = _in[12] + _x;
-		_mat[13] = _in[13] + _y;
-		_mat[14] = _in[14] + _z;
+	Mat4& operator*=(const Mat4& _other) {
+		_Mat4::Multiply(m, m, _other.m);
+		return *this;
 	}
-	void RotateZ(float* _mat, float _rad) {
-		float s = std::sin(_rad);
-		float c = std::cos(_rad);
-		float in00 = _mat[0];
-		float in01 = _mat[1];
-		float in02 = _mat[2];
-		float in03 = _mat[3];
-		float in10 = _mat[4];
-		float in11 = _mat[5];
-		float in12 = _mat[6];
-		float in13 = _mat[7];
+	Mat4 operator*(const Mat4& _other) const {
+		Mat4 temp;
+		_Mat4::Multiply(temp.m, m, _other.m);
+		return temp;
+	}
+	
+	static Mat4 Transpose(const Mat4& _in) {
+		Mat4 temp = _in;
+		_Mat4::Transpose(temp.m);
+		return temp;
+	}
+	static Mat4 Perspective(float _fov, float _aspect, float _near, float _far) {
+		Mat4 temp;
+		_Mat4::Perspective(temp.m, _fov, _aspect, _near, _far);
+		return temp;
+	}
+	static Mat4 Orthographic(float _left, float _right, float _bottom, float _top, float _near, float _far) {
+		Mat4 temp;
+		_Mat4::Orthographic(temp.m, _left, _right, _bottom, _top, _near, _far);
+		return temp;
+	}
+	static Mat4 Translate(float _x, float _y, float _z) {
+		Mat4 temp;
+		_Mat4::Translate(temp.m, _x, _y, _z);
+		return temp;
+	}
+	static Mat4 RotateZ(float _rad) {
+		Mat4 temp;
+		_Mat4::RotateZ(temp.m, _rad);
+		return temp;
+	}
+	static Mat4 RotateY(float _rad) {
+		Mat4 temp;
+		_Mat4::RotateY(temp.m, _rad);
+		return temp;
+	}
+	static Mat4 RotateX(float _rad) {
+		Mat4 temp;
+		_Mat4::RotateX(temp.m, _rad);
+		return temp;
+	}
+	static Mat4 Scale(float _x, float _y, float _z) {
+		Mat4 temp;
+		_Mat4::Scale(temp.m, _x, _y, _z);
+		return temp;
+	}
+	static Mat4 Scale(float _s) {
+		Mat4 temp;
+		_Mat4::Scale(temp.m, _s, _s, _s);
+		return temp;
+	}
+public:
+	float m[16];		
+};
 
-		_mat[0] = in00 * c + in10 * s;
-		_mat[1] = in01 * c + in11 * s;
-		_mat[2] = in02 * c + in12 * s;
-		_mat[3] = in03 * c + in13 * s;
-		_mat[4] = in10 * c - in00 * s;
-		_mat[5] = in11 * c - in01 * s;
-		_mat[6] = in12 * c - in02 * s;
-		_mat[7] = in13 * c - in03 * s;
-	}
-	void RotateZ(float* _mat, const float* _in, float _rad) {
-		float s = std::sin(_rad);
-		float c = std::cos(_rad);
-		float in00 = _in[0];
-		float in01 = _in[1];
-		float in02 = _in[2];
-		float in03 = _in[3];
-		float in10 = _in[4];
-		float in11 = _in[5];
-		float in12 = _in[6];
-		float in13 = _in[7];
-
-		_mat[8] = _in[8];
-		_mat[9] = _in[9];
-		_mat[10] = _in[10];
-		_mat[11] = _in[11];
-		_mat[12] = _in[12];
-		_mat[13] = _in[13];
-		_mat[14] = _in[14];
-		_mat[15] = _in[15];
-
-		_mat[0] = in00 * c + in10 * s;
-		_mat[1] = in01 * c + in11 * s;
-		_mat[2] = in02 * c + in12 * s;
-		_mat[3] = in03 * c + in13 * s;
-		_mat[4] = in10 * c - in00 * s;
-		_mat[5] = in11 * c - in01 * s;
-		_mat[6] = in12 * c - in02 * s;
-		_mat[7] = in13 * c - in03 * s;
-	}
-	void RotateY(float* _mat, float _rad) {
-		float s = std::sin(_rad);
-		float c = std::cos(_rad);
-		float in00 = _mat[0];
-		float in01 = _mat[1];
-		float in02 = _mat[2];
-		float in03 = _mat[3];
-		float in20 = _mat[8];
-		float in21 = _mat[9];
-		float in22 = _mat[10];
-		float in23 = _mat[11];
-
-		_mat[0] = in00 * c - in20 * s;
-		_mat[1] = in01 * c - in21 * s;
-		_mat[2] = in02 * c - in22 * s;
-		_mat[3] = in03 * c - in23 * s;
-		_mat[8] = in00 * s + in20 * c;
-		_mat[9] = in01 * s + in21 * c;
-		_mat[10] = in02 * s + in22 * c;
-		_mat[11] = in03 * s + in23 * c;
-	}
-	void RotateY(float* _mat, const float* _in, float _rad) {
-		float s = std::sin(_rad);
-		float c = std::cos(_rad);
-		float in00 = _in[0];
-		float in01 = _in[1];
-		float in02 = _in[2];
-		float in03 = _in[3];
-		float in20 = _in[8];
-		float in21 = _in[9];
-		float in22 = _in[10];
-		float in23 = _in[11];
-
-		_mat[4] = _in[4];
-		_mat[5] = _in[5];
-		_mat[6] = _in[6];
-		_mat[7] = _in[7];
-		_mat[12] = _in[12];
-		_mat[13] = _in[13];
-		_mat[14] = _in[14];
-		_mat[15] = _in[15];
-		
-		_mat[0] = in00 * c - in20 * s;
-		_mat[1] = in01 * c - in21 * s;
-		_mat[2] = in02 * c - in22 * s;
-		_mat[3] = in03 * c - in23 * s;
-		_mat[8] = in00 * s + in20 * c;
-		_mat[9] = in01 * s + in21 * c;
-		_mat[10] = in02 * s + in22 * c;
-		_mat[11] = in03 * s + in23 * c;
-	}
-	void RotateX(float* _mat, float _rad) {
-		float s = std::sin(_rad);
-		float c = std::cos(_rad);
-		float in10 = _mat[4];
-		float in11 = _mat[5];
-		float in12 = _mat[6];
-		float in13 = _mat[7];
-		float in20 = _mat[8];
-		float in21 = _mat[9];
-		float in22 = _mat[10];
-		float in23 = _mat[11];
-
-		_mat[4] = in10 * c + in20 * s;
-		_mat[5] = in11 * c + in21 * s;
-		_mat[6] = in12 * c + in22 * s;
-		_mat[7] = in13 * c + in23 * s;
-		_mat[8] = in20 * c - in10 * s;
-		_mat[9] = in21 * c - in11 * s;
-		_mat[10] = in22 * c - in12 * s;
-		_mat[11] = in23 * c - in13 * s;
-	}
-	void RotateX(float* _mat, const float* _in, float _rad) {
-		float s = std::sin(_rad);
-		float c = std::cos(_rad);
-		float in10 = _in[4];
-		float in11 = _in[5];
-		float in12 = _in[6];
-		float in13 = _in[7];
-		float in20 = _in[8];
-		float in21 = _in[9];
-		float in22 = _in[10];
-		float in23 = _in[11];
-
-		_mat[0] = _in[0];
-		_mat[1] = _in[1];
-		_mat[2] = _in[2];
-		_mat[3] = _in[3];
-		_mat[12] = _in[12];
-		_mat[13] = _in[13];
-		_mat[14] = _in[14];
-		_mat[15] = _in[15];
-
-		_mat[4] = in10 * c + in20 * s;
-		_mat[5] = in11 * c + in21 * s;
-		_mat[6] = in12 * c + in22 * s;
-		_mat[7] = in13 * c + in23 * s;
-		_mat[8] = in20 * c - in10 * s;
-		_mat[9] = in21 * c - in11 * s;
-		_mat[10] = in22 * c - in12 * s;
-		_mat[11] = in23 * c - in13 * s;
-	}
-	void Scale(float* _mat, float _x, float _y, float _z) {
-		_mat[0] *= _x;
-		_mat[5] *= _y;
-		_mat[10] *= _z;
-	}
-	void Scale(float* _mat, const float* _in, float _x, float _y, float _z) {
-		memcpy(_mat, _in, 16 * sizeof(float));
-		_mat[0] *= _x;
-		_mat[5] *= _y;
-		_mat[10] *= _z;
-	}
-}
-}
-
-#endif/*_ORE_MAT4_HPP_HEADER_FILE_GUARD*/
+#endif/*_ORE__MAT4_HPP_HEADER_FILE_GUARD*/
