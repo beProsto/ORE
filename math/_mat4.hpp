12f0a26454ac717ada76edd4eb414a6464f91264
beProsto <lavionperavion2@gmail.com>
2020-04-17 17:34:25 +0200

    Some developement on inversion

--- math/_mat4.hpp
+++ math/_mat4.hpp
@@ -31,24 +31,24 @@ namespace _Mat4 {
 		float det = 0.0f;
 		int i = 0;
 
-		inv[0] =  _mat[5] *  _mat[10] *  _mat[15] -  _mat[5] *  _mat[11] *  _mat[14] -  _mat[9] *  _mat[6] *  _mat[15] +  _mat[9] *  _mat[7] *  _mat[14] +  _mat[13] *  _mat[6] *  _mat[11] -  _mat[13] *  _mat[7] *  _mat[10];
-		inv[4] = - _mat[4] *  _mat[10] *  _mat[15] +  _mat[4] *  _mat[11] *  _mat[14] +  _mat[8] *  _mat[6] *  _mat[15] -  _mat[8] *  _mat[7] *  _mat[14] -  _mat[12] *  _mat[6] *  _mat[11] +  _mat[12] *  _mat[7] *  _mat[10];
-		inv[8] =  _mat[4] *  _mat[9] *  _mat[15] -  _mat[4] *  _mat[11] *  _mat[13] -  _mat[8] *  _mat[5] *  _mat[15] +  _mat[8] *  _mat[7] *  _mat[13] +  _mat[12] *  _mat[5] *  _mat[11] -  _mat[12] *  _mat[7] *  _mat[9];
-		inv[12] = - _mat[4] *  _mat[9] *  _mat[14] +  _mat[4] *  _mat[10] *  _mat[13] + _mat[8] *  _mat[5] *  _mat[14] -  _mat[8] *  _mat[6] *  _mat[13] -  _mat[12] *  _mat[5] *  _mat[10] +  _mat[12] *  _mat[6] *  _mat[9];
-		inv[1] = - _mat[1] *  _mat[10] *  _mat[15] +  _mat[1] *  _mat[11] *  _mat[14] +  _mat[9] *  _mat[2] *  _mat[15] -  _mat[9] *  _mat[3] *  _mat[14] -  _mat[13] *  _mat[2] *  _mat[11] +  _mat[13] *  _mat[3] *  _mat[10];
-		inv[5] =  _mat[0] *  _mat[10] *  _mat[15] -  _mat[0] *  _mat[11] *  _mat[14] -  _mat[8] *  _mat[2] *  _mat[15] +  _mat[8] *  _mat[3] *  _mat[14] +  _mat[12] *  _mat[2] *  _mat[11] -  _mat[12] *  _mat[3] *  _mat[10];
-		inv[9] = - _mat[0] *  _mat[9] *  _mat[15] +  _mat[0] *  _mat[11] *  _mat[13] +  _mat[8] *  _mat[1] *  _mat[15] -  _mat[8] *  _mat[3] *  _mat[13] -  _mat[12] *  _mat[1] *  _mat[11] +  _mat[12] *  _mat[3] *  _mat[9];
-		inv[13] =  _mat[0] *  _mat[9] *  _mat[14] -  _mat[0] *  _mat[10] *  _mat[13] -  _mat[8] *  _mat[1] *  _mat[14] +  _mat[8] *  _mat[2] *  _mat[13] +  _mat[12] *  _mat[1] *  _mat[10] -  _mat[12] *  _mat[2] *  _mat[9];
-		inv[2] =  _mat[1] *  _mat[6] *  _mat[15] -  _mat[1] *  _mat[7] *  _mat[14] -  _mat[5] *  _mat[2] *  _mat[15] +  _mat[5] *  _mat[3] *  _mat[14] +  _mat[13] *  _mat[2] *  _mat[7] -  _mat[13] *  _mat[3] *  _mat[6];
-		inv[6] = - _mat[0] *  _mat[6] *  _mat[15] +  _mat[0] *  _mat[7] *  _mat[14] +  _mat[4] *  _mat[2] *  _mat[15] -  _mat[4] *  _mat[3] *  _mat[14] -  _mat[12] *  _mat[2] *  _mat[7] +  _mat[12] *  _mat[3] *  _mat[6];
-		inv[10] =  _mat[0] *  _mat[5] *  _mat[15] -  _mat[0] *  _mat[7] *  _mat[13] -  _mat[4] *  _mat[1] *  _mat[15] +  _mat[4] *  _mat[3] *  _mat[13] +  _mat[12] *  _mat[1] *  _mat[7] -  _mat[12] *  _mat[3] *  _mat[5];
-		inv[14] = - _mat[0] *  _mat[5] *  _mat[14] +  _mat[0] *  _mat[6] *  _mat[13] +  _mat[4] *  _mat[1] *  _mat[14] -  _mat[4] *  _mat[2] *  _mat[13] -  _mat[12] *  _mat[1] *  _mat[6] +  _mat[12] *  _mat[2] *  _mat[5];
-		inv[3] = - _mat[1] *  _mat[6] *  _mat[11] +  _mat[1] *  _mat[7] *  _mat[10] +  _mat[5] *  _mat[2] *  _mat[11] -  _mat[5] *  _mat[3] *  _mat[10] -  _mat[9] *  _mat[2] *  _mat[7] +  _mat[9] *  _mat[3] *  _mat[6];
-		inv[7] =  _mat[0] *  _mat[6] *  _mat[11] -  _mat[0] *  _mat[7] *  _mat[10] -  _mat[4] *  _mat[2] *  _mat[11] +  _mat[4] *  _mat[3] *  _mat[10] +  _mat[8] *  _mat[2] *  _mat[7] -  _mat[8] *  _mat[3] *  _mat[6];
-		inv[11] = - _mat[0] *  _mat[5] *  _mat[11] +  _mat[0] *  _mat[7] *  _mat[9] +  _mat[4] *  _mat[1] *  _mat[11] -  _mat[4] *  _mat[3] *  _mat[9] -  _mat[8] *  _mat[1] *  _mat[7] +  _mat[8] *  _mat[3] *  _mat[5];
-		inv[15] =  _mat[0] *  _mat[5] *  _mat[10] -  _mat[0] *  _mat[6] *  _mat[9] -  _mat[4] *  _mat[1] *  _mat[10] +  _mat[4] *  _mat[2] *  _mat[9] +  _mat[8] *  _mat[1] *  _mat[6] -  _mat[8] *  _mat[2] *  _mat[5];
+		inv[0] = _mat[5] * _mat[10] * _mat[15] - _mat[5] * _mat[11] * _mat[14] - _mat[9] * _mat[6] * _mat[15] + _mat[9] * _mat[7] * _mat[14] + _mat[13] * _mat[6] * _mat[11] - _mat[13] * _mat[7] * _mat[10];
+		inv[4] = - _mat[4] * _mat[10] * _mat[15] + _mat[4] * _mat[11] * _mat[14] + _mat[8] * _mat[6] * _mat[15] - _mat[8] * _mat[7] * _mat[14] - _mat[12] * _mat[6] * _mat[11] + _mat[12] * _mat[7] * _mat[10];
+		inv[8] = _mat[4] * _mat[9] * _mat[15] - _mat[4] * _mat[11] * _mat[13] - _mat[8] * _mat[5] * _mat[15] + _mat[8] * _mat[7] * _mat[13] + _mat[12] * _mat[5] * _mat[11] - _mat[12] * _mat[7] * _mat[9];
+		inv[12] = - _mat[4] * _mat[9] * _mat[14] + _mat[4] * _mat[10] * _mat[13] + _mat[8] * _mat[5] * _mat[14] - _mat[8] * _mat[6] * _mat[13] - _mat[12] * _mat[5] * _mat[10] + _mat[12] * _mat[6] * _mat[9];
+		inv[1] = - _mat[1] * _mat[10] * _mat[15] + _mat[1] * _mat[11] * _mat[14] + _mat[9] * _mat[2] * _mat[15] - _mat[9] * _mat[3] * _mat[14] - _mat[13] * _mat[2] * _mat[11] + _mat[13] * _mat[3] * _mat[10];
+		inv[5] = _mat[0] * _mat[10] * _mat[15] - _mat[0] * _mat[11] * _mat[14] - _mat[8] * _mat[2] * _mat[15] + _mat[8] * _mat[3] * _mat[14] + _mat[12] * _mat[2] * _mat[11] - _mat[12] * _mat[3] * _mat[10];
+		inv[9] = - _mat[0] * _mat[9] * _mat[15] + _mat[0] * _mat[11] * _mat[13] + _mat[8] * _mat[1] * _mat[15] - _mat[8] * _mat[3] * _mat[13] - _mat[12] * _mat[1] * _mat[11] + _mat[12] * _mat[3] * _mat[9];
+		inv[13] = _mat[0] * _mat[9] * _mat[14] - _mat[0] * _mat[10] * _mat[13] - _mat[8] * _mat[1] * _mat[14] + _mat[8] * _mat[2] * _mat[13] + _mat[12] * _mat[1] * _mat[10] - _mat[12] * _mat[2] * _mat[9];
+		inv[2] = _mat[1] * _mat[6] * _mat[15] - _mat[1] * _mat[7] * _mat[14] - _mat[5] * _mat[2] * _mat[15] + _mat[5] * _mat[3] * _mat[14] + _mat[13] * _mat[2] * _mat[7] - _mat[13] * _mat[3] * _mat[6];
+		inv[6] = - _mat[0] * _mat[6] * _mat[15] + _mat[0] * _mat[7] * _mat[14] + _mat[4] * _mat[2] * _mat[15] - _mat[4] * _mat[3] * _mat[14] - _mat[12] * _mat[2] * _mat[7] + _mat[12] * _mat[3] * _mat[6];
+		inv[10] = _mat[0] * _mat[5] * _mat[15] - _mat[0] * _mat[7] * _mat[13] - _mat[4] * _mat[1] * _mat[15] + _mat[4] * _mat[3] * _mat[13] + _mat[12] * _mat[1] * _mat[7] - _mat[12] * _mat[3] * _mat[5];
+		inv[14] = - _mat[0] * _mat[5] * _mat[14] + _mat[0] * _mat[6] * _mat[13] + _mat[4] * _mat[1] * _mat[14] - _mat[4] * _mat[2] * _mat[13] - _mat[12] * _mat[1] * _mat[6] + _mat[12] * _mat[2] * _mat[5];
+		inv[3] = - _mat[1] * _mat[6] * _mat[11] + _mat[1] * _mat[7] * _mat[10] + _mat[5] * _mat[2] * _mat[11] - _mat[5] * _mat[3] * _mat[10] - _mat[9] * _mat[2] * _mat[7] + _mat[9] * _mat[3] * _mat[6];
+		inv[7] = _mat[0] * _mat[6] * _mat[11] - _mat[0] * _mat[7] * _mat[10] - _mat[4] * _mat[2] * _mat[11] + _mat[4] * _mat[3] * _mat[10] + _mat[8] * _mat[2] * _mat[7] - _mat[8] * _mat[3] * _mat[6];
+		inv[11] = - _mat[0] * _mat[5] * _mat[11] + _mat[0] * _mat[7] * _mat[9] + _mat[4] * _mat[1] * _mat[11] - _mat[4] * _mat[3] * _mat[9] - _mat[8] * _mat[1] * _mat[7] + _mat[8] * _mat[3] * _mat[5];
+		inv[15] = _mat[0] * _mat[5] * _mat[10] - _mat[0] * _mat[6] * _mat[9] - _mat[4] * _mat[1] * _mat[10] + _mat[4] * _mat[2] * _mat[9] + _mat[8] * _mat[1] * _mat[6] - _mat[8] * _mat[2] * _mat[5];
 
-		det =  _mat[0] * inv[0] +  _mat[1] * inv[4] +  _mat[2] * inv[8] +  _mat[3] * inv[12];
+		det = _mat[0] * inv[0] + _mat[1] * inv[4] + _mat[2] * inv[8] + _mat[3] * inv[12];
 		if(det == 0) return;
 		det = 1.0 / det;
 
